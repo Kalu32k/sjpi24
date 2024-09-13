7fddd62309f585620d2cf7cae425c2b3e6a2bf3d
Calle <ca.karlsson@hotmail.com>
2024-09-13 16:22:58 +0200

    assignment 3

---
+++ uppgift3.js
@@ -0,0 +1,18 @@
+function changeHeader(){
+    const myHeader= document.querySelector("h1")
+    myHeader.innerText="Goodbye World"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const myH1=document.querySelector("body");
+// console.log(myH1);
