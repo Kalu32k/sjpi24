bb3a248371c6f48d441faf2d70e385280e4ff982
Calle <ca.karlsson@hotmail.com>
2024-09-13 17:21:52 +0200

    code cleanup

--- uppgift3.js
+++ uppgift3.js
@@ -2,17 +2,3 @@ function changeHeader(){
     const myHeader= document.querySelector("h1")
     myHeader.innerText="Goodbye World"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// const myH1=document.querySelector("body");
-// console.log(myH1);
